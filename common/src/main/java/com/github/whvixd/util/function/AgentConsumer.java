59fe49241ea7ce94f57697cc4343698a28ae4b77
wangzhx <wangzhx@fxiaoke.com>
2019-07-22 19:25:53 +0800

    add AgentConsumer Function

--- common/src/main/java/com/github/whvixd/util/function/AgentConsumer.java
+++ common/src/main/java/com/github/whvixd/util/function/AgentConsumer.java
@@ -1,9 +1,16 @@
 package com.github.whvixd.util.function;
 
+import java.util.Objects;
+
 /**
  * Created by wangzhx on 2019/7/22.
  */
 @FunctionalInterface
 public interface AgentConsumer {
     void accept();
+
+    default AgentConsumer andThen(AgentConsumer agentConsumer) {
+        Objects.requireNonNull(agentConsumer);
+        return ()->{accept(); agentConsumer.accept();};
+    }
 }
