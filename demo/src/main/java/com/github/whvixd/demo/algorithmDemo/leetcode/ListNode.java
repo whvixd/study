4ecde3c209856f783f824b46ebd72017e98e4375
wangzhx <wangzhx@fxiaoke.com>
2020-03-24 13:39:59 +0800

    leetcode 03-24 1 合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。

--- demo/src/main/java/com/github/whvixd/demo/algorithmDemo/leetcode/ListNode.java
+++ demo/src/main/java/com/github/whvixd/demo/algorithmDemo/leetcode/ListNode.java
@@ -4,22 +4,39 @@ package com.github.whvixd.demo.algorithmDemo.leetcode;
  * Created by wangzhx on 2020/3/21.
  */
 public class ListNode {
-    int val;
+     int val;
     ListNode next;
 
     ListNode(int x) {
         val = x;
     }
 
-    public static void print(ListNode l) {
+    public static void print(ListNode l, String preMessage, String postMessage) {
+        preMessage=preMessage==null?"":preMessage;
+        postMessage=postMessage==null?"":postMessage;
         ListNode head = l;
+        StringBuilder sb = new StringBuilder();
+        sb.append(preMessage);
         while (head != null) {
-            System.out.print(head.val);
+            sb.append(head.val);
             head = head.next;
             if (head != null) {
-                System.out.print("->");
+                sb.append("->");
             }
         }
+        sb.append(postMessage);
+        System.out.println(sb);
+    }
 
+    public static void print(ListNode l) {
+        print(l, null, null);
+    }
+
+    public static void print(String preMessage, ListNode l) {
+        print(l, preMessage, null);
+    }
+
+    public static void print(ListNode l, String postMessage) {
+        print(l, null, postMessage);
     }
 }
