6375f124bc551bf428e824e450a10c3b1edfd274
whvixd <631791301@qq.com>
2020-04-18 15:46:58 +0800

    添加ABA问题处理方式

--- demo/src/test/java/com/github/whvixd/testDemo/MockServer.java
+++ demo/src/test/java/com/github/whvixd/testDemo/MockServer.java
@@ -1,12 +1,5 @@
 package com.github.whvixd.testDemo;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
-import spark.resource.ClassPathResource;
-
-import java.io.IOException;
-
-import static sun.rmi.rmic.Names.stubFor;
 
 /**
  * Created by wangzhx on 2019/3/4.
