5dc2898c0af0caf5591270171149e3c610bd0be2
wangzhx <wangzhx@fxiaoke.com>
2020-05-20 16:18:12 +0800

    leetcode 面试题 02.03	删除中间节点

--- demo/src/main/java/com/github/whvixd/demo/algorithm/leetcode/easy/Q0203.java
+++ demo/src/main/java/com/github/whvixd/demo/algorithm/leetcode/easy/Q0203.java
@@ -27,11 +27,6 @@ public enum Q0203 {
     public static void main(String[] args) {
         int[] array=new int[]{1,2,3,4,5};
         ListNode node = ListNode.createByArray(array);
-
-//        ListNode head=new ListNode(1);
-//        head.next=new ListNode(2);
-//        head.next.next=new ListNode(3);
-//        head.next.next.next=new ListNode(4);
         ListNode.print("before:",node);
         Q0203.instance.deleteNode(node.next.next);
         ListNode.print("after:",node);
