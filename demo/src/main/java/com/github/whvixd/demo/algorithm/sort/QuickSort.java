3215a31cdfe98cf365b2252f7de1c12133cd863e
whvixd <631791301@qq.com>
2020-05-30 09:52:48 +0800

    leetcode 84. 柱状图中最大的矩形

--- demo/src/main/java/com/github/whvixd/demo/algorithm/sort/QuickSort.java
+++ demo/src/main/java/com/github/whvixd/demo/algorithm/sort/QuickSort.java
@@ -2,16 +2,17 @@ package com.github.whvixd.demo.algorithm.sort;
 
 /**
  * 取基数，一次交换使得  左<基数<右 左右不一定有序
+ * 升序先从右侧遍历
  * Created by wangzhx on 2020/4/21.
  */
 public class QuickSort {
 
-    void sort(int[] originArray) {
+    public static void sort(int[] originArray) {
         int length = originArray.length;
         sort(originArray, 0, length-1);
     }
 
-    private void sort(int[] originArray, int left, int right) {
+    private static void sort(int[] originArray, int left, int right) {
         if(left>=right){
             return;
         }
@@ -19,13 +20,14 @@ public class QuickSort {
         int pivot = originArray[left];
         int i = left,j=right;
         while (i<j){
-            // 从左边找到比基数小的
-            while (originArray[i]<=pivot&&i<j){
-                i++;
-            }
             // 从右边找到比基数大的
             while (originArray[j]>=pivot&&i<j){
                 j--;
+            }
+
+            // 从左边找到比基数小的
+            while (originArray[i]<=pivot&&i<j){
+                i++;
             }
 
             if(i<j){
@@ -40,8 +42,8 @@ public class QuickSort {
         originArray[left]= originArray[i];
         originArray[i]=pivot;
         // 递归 之后所有基数升序
-        sort(originArray, left, i-1);
-        sort(originArray, i+1, right);
+        sort(originArray, left, i);
+        sort(originArray, j+1, right);
     }
 
 }
