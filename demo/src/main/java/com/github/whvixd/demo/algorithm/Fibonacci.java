ccabc0646eb3ebfbe4dea9c44c78749278ca0b7a
whvixd <631791301@qq.com>
2020-06-01 14:50:04 +0800

    rename

--- demo/src/main/java/com/github/whvixd/demo/algorithm/Fibonacci.java
+++ demo/src/main/java/com/github/whvixd/demo/algorithm/Fibonacci.java
@@ -5,14 +5,14 @@ package com.github.whvixd.demo.algorithm;
  *
  * Created by wangzhx on 2020/4/19.
  */
-public class F {
-    public int f(int n){
+public class Fibonacci {
+    public int invoke(int n){
         if(n<=0){
             return 0;
         }
         if(n<=2){
             return n;
         }
-        return f(n-1)+f(n-2);
+        return invoke(n-1)+invoke(n-2);
     }
 }
