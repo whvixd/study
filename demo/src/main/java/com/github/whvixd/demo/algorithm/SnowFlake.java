3b93cf23d183a199e9818fc55a96138bfc44b3c6
wangzhixiang <wangzhixiang_v@didichuxing.com>
2020-08-10 16:36:41 +0800

    优化

--- demo/src/main/java/com/github/whvixd/demo/algorithm/SnowFlake.java
+++ demo/src/main/java/com/github/whvixd/demo/algorithm/SnowFlake.java
@@ -10,7 +10,7 @@ public class SnowFlake {
     /**
      * 起始的时间戳
      */
-    private final static long START_STAMP = 1480166465631L;
+    private final static long START_STAMP = 785147713637L;
 
     /**
      * 每一部分占用的位数
@@ -22,9 +22,9 @@ public class SnowFlake {
     /**
      * 每一部分的最大值
      */
-    private final static long MAX_DATA_CENTER_NUM = -1L ^ (-1L << DATA_CENTER_BIT);
-    private final static long MAX_MACHINE_NUM = -1L ^ (-1L << MACHINE_BIT);
-    private final static long MAX_SEQUENCE = -1L ^ (-1L << SEQUENCE_BIT);
+    private final static long MAX_DATA_CENTER_NUM = ~(-1L << DATA_CENTER_BIT);
+    private final static long MAX_MACHINE_NUM = ~(-1L << MACHINE_BIT);
+    private final static long MAX_SEQUENCE = ~(-1L << SEQUENCE_BIT);
 
     /**
      * 每一部分向左的位移
