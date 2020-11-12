63f14926aabd0f0a79930320e9af00d8f6f960b6
wangzhixiang <wangzhixiang_v@didichuxing.com>
2020-11-12 19:38:35 +0800

    指针，结构体

--- c/base/test_point.c
+++ c/base/test_point.c
@@ -20,12 +20,28 @@ int main()
 
     printf("a:%d \n",*ap);
 
-    ////////////////////
+    printf("/////////空指针////////\n");
     // NULL：空指针 =.=! //
     // 0x0 内存0地址，系统保留地址，程序不可访问
     int *null_point=NULL;
     printf("NULL:%p\n",null_point);
     if(!null_point) printf("空\n");
-    if(null_point) printf("非空\n");
+    if(null_point) printf("非空\n\n");
+
+    printf("/////////指针、地址、内容/////////\n");
+    int x=3;
+    int *px=&x;
+    int **ppx=&px;
+    printf("x:%d\n",x);//x内容 => 3
+    printf("&x:%p\n\n",&x);//x地址
+
+    printf("px:%p\n",px);//px内容 => x地址
+    printf("&px:%p\n",&px);//px地址
+    printf("*px:%d\n\n",*px);//px指向地址的内容 => x内容
+
+    printf("ppx:%p\n",ppx);//ppx内容 => px地址
+    printf("&ppx:%p\n",&ppx);//ppx地址
+    printf("*ppx:%p\n",*ppx);//ppx指向地址的内容 => px内容 => x地址
+    printf("**ppx:%d\n",**ppx);//ppx指向地址的内容指向的地址内容 =>x内容
 
 }
--- c/base/test_struct.c
+++ c/base/test_struct.c
@@ -2,12 +2,33 @@
 # include <string.h>
 # include <stdlib.h>
 
+// typedef 给变量添加别名,可以添加多个别名
+// typedef type type_alias1,*type_alias2
 typedef struct Student
 {
     long id;
     char name[10];
     int age;
 }Stu;
+
+typedef int Integer,Number;
+
+// 全局的结构体变量
+struct Teacher
+{
+    long id;
+    char name[10];
+//    char subject[10];
+//    long stu_ids[50];
+} tea3={3l,"tea Z"};
+
+/////////////位域///////////////
+struct Data{
+    unsigned a:4;//a占四位 2^3+2^2+2^1+2^0 [0,15]
+    unsigned  :4;//空域，该四位不存值
+    unsigned b:1;//b一位
+    unsigned c:7;//c七位
+} data,*p_data;
 
 int main()
 {
@@ -23,7 +44,7 @@ int main()
 
     // stu2 是对象，访问成员变量用 .
     // *stu3 是指针，访问成员变量用 ->
-    Stu *stu3=(Stu *)malloc(sizeof(Stu));
+    Stu *stu3=(Stu *)malloc(sizeof(Stu)); //malloc 函数返回的是 void * ，需要强转我们需要的类型
     (*stu3).id=3;
     strcpy(stu3->name,"Jack");
     stu3->age=24;
@@ -35,6 +56,35 @@ int main()
     strcpy(stu5->name,"BigJ");
     stu5->age=25;
     printf("%ld %s %d\n",(*stu5).id,(*stu5).name,(*stu5).age);
+
+    struct Teacher tea1={1l,"tea wang"};
+    printf("tea1.id:%ld,tea1.name:%s\n",tea1.id,tea1.name);
+    struct Teacher *tea2=NULL;
+    printf("tea2:%p\n",tea2);
+    printf("tea3.id:%ld,tea3.name:%s\n",tea3.id,tea3.name);
+
+    printf("/////////局部结构体/////////\n");
+    struct Book
+    {
+        long id;
+        char name[10];
+    } bo={1l,"Java"};
+    printf("bo.id:%ld,bo.name:%s\n",bo.id,bo.name);
+    printf("//////////////////\n");
+
+    Integer int1=3;
+    printf("Integer int1:%d\n",int1);
+    Number num=4;
+    printf("Number num:%d\n",num);
+
+    data.a=15;
+    data.b=1;
+    data.c='c';
+    printf("data.a:%d,data.b:%d,data.c:%c\n",data.a,data.b,data.c);
+
+    p_data=&data;
+    p_data->a=2;
+    printf("p_data->a:%d\n",p_data->a);
 
 
 }
