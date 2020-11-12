b723f86d8d859d33f512bfb69d4bdadf89a26ebd
wangzhixiang <wangzhixiang_v@didichuxing.com>
2020-11-12 16:52:19 +0800

    fun point and defalut value

--- c/base/test_fun_point.c
+++ c/base/test_fun_point.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#define ARRAY_SIZE 10
+
+int get_random()
+{
+    return rand();
+}
+
+void set_array(int *array,int array_size,int (*get_ele)(void))
+{
+    for(int i=0;i<array_size;i++){
+        array[i]=get_ele();
+    }
+}
 
 int equal(int a,int b)
 {
@@ -17,11 +31,28 @@ int equal(int a,int b)
 
 typedef int (*fun_ptr)(int,int); // 声明一个指向同样参数、返回值的函数指针类型
 */
+
+int d;
 int main()
 {
-    // 函数指针
+    //////////// 函数指针 ////////////
     int (*fun_equal)(int,int)=&equal;// &可省略
     int r=fun_equal(fun_equal(1,1),2);
     printf("%d\n",r);
+
+    int array[ARRAY_SIZE];
+    printf("before array[0]:%d\n",array[0]);
+    set_array(array,ARRAY_SIZE,get_random);
+    printf("after array[0]:%d\n",array[0]);
+
+    ////////////默认值////////////
+    // 局部变量随机值，
+    // 静态变量和全局变量默认值为0，
+    int b;
+    static int c;
+    int e[];
+    printf("b:%d\n",b);
+    printf("static c:%d\n",c);
+    printf("e[0]:%d\n",e[0]);
 
 }
