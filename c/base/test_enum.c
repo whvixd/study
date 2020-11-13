c85f8a9016f27428a0d583a0bd1cf29073d8c420
wangzhixiang <wangzhixiang_v@didichuxing.com>
2020-11-13 13:41:49 +0800

    switch enum

--- c/base/test_enum.c
+++ c/base/test_enum.c
@@ -12,4 +12,20 @@ int main(void)
     printf("D1:%c\n",D1);
     printf("D2:%d\n",D2);
     printf("D3:%dl\n",D3);
+
+    day=D2;
+    switch(day)
+    {
+        case D1:
+            printf("select D1\n");
+            break;
+        case D2:
+            printf("select D2\n");
+            break;
+        case D3:
+            printf("select D3\n");
+            break;
+        default:
+            printf("default");
+    }
 }
