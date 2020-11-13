16d1e4ebb3c03f335b5cc404aaf3d7f2df3c7a2a
wangzhixiang <wangzhixiang_v@didichuxing.com>
2020-11-13 18:39:01 +0800

    socket

--- c/system/sendfile.c
+++ c/system/sendfile.c
@@ -1,7 +1,13 @@
+#include <stdio.h>
 #include <sys/socket.h>
 
 int main()
 {
-    sendfile(1,1,1,1);
+    // ssize_t sendfile(int out_fd, int in_fd, off_t *offset, size_t count);
+    FILE *p_in=fopen("/Users/didi/Documents/workspace/idea/study/c/debug/in.txt","r+");
+    FILE *p_out=fopen("/Users/didi/Documents/workspace/idea/study/c/debug/out.txt","w+");
+
+    int size=sendfile(p_out,p_in,1024,1024);
+    printf("size:%d",size);
     return 0;
 }
