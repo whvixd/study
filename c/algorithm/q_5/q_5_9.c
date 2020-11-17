298c1e8d2a66cb3fa0cce546c76ac88655ca95e6
wangzhixiang <wangzhixiang_v@didichuxing.com>
2020-11-17 17:13:57 +0800

    找出1000之内所有完数 -- q_5_9.c

---
+++ c/algorithm/q_5/q_5_9.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// Q:一个数如果因子之和等于这个数，这个数称完数，如6=1+2+3
+// 找出1000之内所有完数
+int main()
+{
+    int m,s,i;
+    for(m=2;m<1000;m++){
+        s=0;
+        for(i=1;i<m;i++){
+            if(m%i==0){
+                s+=i;
+            }
+
+            if(s==m){
+                printf("%d,its factors are ",m);
+                for(i=1;i<m;i++){
+                    if(m%i==0){
+                        printf("%d ",i);
+                    }
+                }
+                printf("\n");
+
+            }
+
+        }
+    }
+    return 0;
+}
