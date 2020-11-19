9facd25ec420af41830be7cefb667e7aa446b28f
wangzhixiang <wangzhixiang_v@didichuxing.com>
2020-11-19 15:52:14 +0800

    q_5_13.c

--- c/algorithm/q_5/q_5_13.c
+++ c/algorithm/q_5/q_5_13.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
 // Q:用迭代法求x=根号a,求平方根的迭代公式为 x2=1/2 (x1+a/x1)
 // 要求前后两次求出的x的差绝对值小于10^-5
 int main()
 {
-    float a,x1,x2;
+    float a,x0,x1;
     printf("输出一个数字:");
     scanf("%f",&a);
 
@@ -13,6 +14,7 @@ int main()
     do{
         x0=x1;
         x1=(x0+a/x0)/2;
-    }while(fabs(x0-x1)>=le-5);
+    }while(fabs(x0-x1)>=1e-5);//10^-5
+    printf("The square root of %5.2f is %8.5f\n",a,x1);
     return 0;
 }
