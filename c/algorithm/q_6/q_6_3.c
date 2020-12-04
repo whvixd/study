18073d38f234027aa1176aefc0a6f90981083d97
wangzhixiang <wangzhixiang_v@didichuxing.com>
2020-12-04 18:57:12 +0800

    q_6_3.c

--- c/algorithm/q_6/q_6_3.c
+++ c/algorithm/q_6/q_6_3.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-//void print_matrix(int matrix[][3],int size);
 int get_rand();
-
 
 // Q:求一个3X3的整数矩阵对角线之和
 int main(){
+    void print_matrix(int matrix[][3]);
     int matrix[3][3];
 
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             matrix[i][j]=get_rand();
-            printf("%d ",matrix[i][j]);
         }
-        printf("\n");
     }
 
     int sum=0;
     for(int i=0;i<3;i++){
         sum+=matrix[i][i];
     }
+    print_matrix(matrix);
+
     printf("sum:%d\n ",sum);
+
     return 0;
 }
 
@@ -29,15 +28,15 @@ int get_rand(){
     return rand()%100;
 }
 
-// todo 二维数组传参
-//void print_matrix(int matrix[][3],int size){
-//    for(int i=0;i<3;i++){
-//        for(int j=0;j<3;j++){
-//            int v=matrix[i][j];
-//            printf("%d ",v);
-//        }
-//        printf("\n");
-//    }
-//
-//    printf("\n");
-//}
+// 二维数组传参
+void print_matrix(int matrix[][3]){
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            int v=matrix[i][j];
+            printf("%d ",v);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+}
