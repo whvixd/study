01368d2a1afe471435ee691f40f2564772ca82cc
wangzhixiang <wangzhixiang_v@didichuxing.com>
2020-12-08 16:44:54 +0800

    extern or static func

--- c/base/test_extern_1.c
+++ c/base/test_extern_1.c
@@ -1,5 +1,13 @@
-#include <stdio.h>
+#include <stdio.h> // 原型
 
+//  添加 static，则函数为内部函数，不允许外部文件调用
+static test_static(){
+}
+
+// 添加extern，则函数为外部函数，可被本文件及外部文件调用，不加默认是外部函数
+extern test_extern(){}
+
+// static int A; 只允许本文件使用，即使外部文件使用了 extern也不能使用
 int A;
 // gcc test_extern_1.c test_extern_2.c -o test_extern
 // ./extern
