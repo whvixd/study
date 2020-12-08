e927939c100af5df91fff7acac2d61b768f7fbc1
wangzhixiang <wangzhixiang_v@didichuxing.com>
2020-12-08 15:49:59 +0800

    extern

--- c/base/test_extern_1.c
+++ c/base/test_extern_1.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
 
 int A;
+// gcc test_extern_1.c test_extern_2.c -o test_extern
+// ./extern
+int main(){
+    void print_A();
+
+    A=10;
+    print_A();
+    return 0;
+}
---
+++ c/base/test_extern_2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+extern int A;
+
+void print_A(){
+    printf("print_A:%d\n",A);
+}
