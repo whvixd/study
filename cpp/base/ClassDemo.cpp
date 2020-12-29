1ef585269e67245df473f4db11e671de07ad5d11
wangzhixiang <wangzhixiang_v@didichuxing.com>
2020-12-29 13:31:39 +0800

    多态、虚函数、纯虚函数

--- cpp/base/ClassDemo.cpp
+++ cpp/base/ClassDemo.cpp
@@ -25,8 +25,16 @@ void Box::set(int l,int b,int h){
 
 class F{
     public:
+        F(int a=0){
+            common=a;
+        }
         void setCommon(int c){
             common=c;
+        }
+        // 不加virtual，若子类中也有该函数，则调用的是父类的；添加之后调用的是子类的
+        // 虚函数，动态链接，后期绑定
+        virtual int getCommon(){
+            return common;
         }
     protected:
         int common;
@@ -34,9 +42,14 @@ class F{
 
 class M{
     public:
+        M(int b=0){
+            number=b;
+        }
         void setNumber(int n){
             number=n;
         }
+        // 纯虚函数
+        virtual int getNumber()=0;
     protected:
         int number;
 };
@@ -44,11 +57,12 @@ class M{
 // 多继承
 class C:public F,public M{
     public:
+        C(int a=0,int b=0):F(a),M(b){}
         int getCommon(){
-            return common;
+            return common+1;
         }
         int getNumber(){
-            return number;
+            return number+1;
         }
 
     // 重载+符
@@ -68,6 +82,7 @@ int main(){
 
     cout<<"Box:"<<v<<endl;
 
+/////多继承/////
     C c1;
     c1.setCommon(4);
     c1.setNumber(5);
@@ -78,9 +93,19 @@ int main(){
     c2.setCommon(6);
     c2.setNumber(7);
 
+/////重载符/////
     C c3=c1+c2;
     cout<<"c3.common:"<<c3.getCommon()<<endl;
     cout<<"c3.number:"<<c3.getNumber()<<endl;
+
+/////多态/////
+    F *f;
+    M *m;
+    C c4(8,9);
+    f=&c4;
+    m=&c4;
+    cout<<"f->getCommon():"<<f->getCommon()<<endl;
+    cout<<"m->getNumber():"<<m->getNumber()<<endl;
 
     return 0;
 }
