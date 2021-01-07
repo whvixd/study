6a407947499aaec2c8eadfe1ddc6eda1e1c18565
wangzhixiang <wangzhixiang_v@didichuxing.com>
2021-01-07 13:13:19 +0800

    FinalizeDemo

--- demo/src/main/java/com/github/whvixd/demo/jvm/FinalizeDemo.java
+++ demo/src/main/java/com/github/whvixd/demo/jvm/FinalizeDemo.java
@@ -1,4 +1,4 @@
-package com.github.whvixd.demo.jdk.util;
+package com.github.whvixd.demo.jvm;
 
 /**
  * Created by wangzhx on 2020/2/25.
@@ -9,15 +9,16 @@ public class FinalizeDemo {
     @Override
     protected void finalize() throws Throwable {
         super.finalize();
+        System.out.println("invoke finalize");
         finalizeDemo = this;
     }
 
-    public static void main(String[] args) {
-        ClassLoader.getSystemClassLoader();
+    public static void main(String[] args) throws InterruptedException {
         finalizeDemo = new FinalizeDemo();
         finalizeDemo = null;
-        System.gc();//会调用finalize
+        System.gc();//会调用finalize,异步执行
 
+        Thread.sleep(500);
         if (finalizeDemo != null) {
             System.out.println("finalizeDemo!=null");
         } else {
@@ -26,6 +27,7 @@ public class FinalizeDemo {
 
         finalizeDemo = null;
         System.gc();
+        Thread.sleep(500);
         if (finalizeDemo != null) {
             System.out.println("finalizeDemo!=null");
         } else {
