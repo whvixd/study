7df68df44656d34f61dc6e66eaa2e3a2c583a64e
wangzhixiang <wangzhixiang_v@didichuxing.com>
2021-02-17 10:54:37 +0800

    park

--- demo/src/main/java/com/github/whvixd/demo/jdk/thread/LockSupportDemo.java
+++ demo/src/main/java/com/github/whvixd/demo/jdk/thread/LockSupportDemo.java
@@ -1,5 +1,7 @@
 package com.github.whvixd.demo.jdk.thread;
 
+import com.github.whvixd.util.InvokeTask;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.locks.LockSupport;
@@ -11,16 +13,27 @@ public class LockSupportDemo {
     private Map<String,Thread> map = new HashMap<>();
     private void lock(){
         map.put("1",Thread.currentThread());
+        System.out.println("park");
         LockSupport.park(this);
+        System.out.println("unpark");
     }
 
     private void unlock(){
-        LockSupport.unpark(map.get("1"));
+        Thread thread = map.get("1");
+
+        LockSupport.unpark(thread);
+
+        Thread.State state = thread.getState();
+        System.out.println("after state:"+state);
+        System.out.println("after isInterrupted:"+thread.isInterrupted());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         LockSupportDemo demo = new LockSupportDemo();
-        demo.lock();
+        InvokeTask.newInstance(()->{
+            demo.lock();
+        }).start();
+        Thread.sleep(1000);
         demo.unlock();
     }
 }
