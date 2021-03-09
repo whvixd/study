8a64a4af9ea09b85db84a1adee197b8b3c031c53
wangzhixiang <wangzhixiang_v@didichuxing.com>
2021-03-09 20:23:44 +0800

    优化

--- demo/src/main/java/com/github/whvixd/demo/jdk/thread/CountDownLatchDemo.java
+++ demo/src/main/java/com/github/whvixd/demo/jdk/thread/CountDownLatchDemo.java
@@ -8,7 +8,7 @@ import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
 /**
- * 等待其他线程-1操作，到0，会执行 await方法
+ * 等待其他线程减一操作，到0，会执行 await方法
  * Created by wangzhx on 2020/3/7.
  */
 public class CountDownLatchDemo {
@@ -21,6 +21,7 @@ public class CountDownLatchDemo {
 
         InvokeTask boss = InvokeTask.newInstance(() -> {
             try {
+                //调用await()方法的线程会被挂起，它会等待直到count值为0才继续执行
                 countDownLatch.await();
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -28,14 +29,16 @@ public class CountDownLatchDemo {
             System.out.println("boss," + countDownLatch.getCount());
         }).invokeTaskName("boss");
 
+        executor.execute(boss);
+
         IntStream.range(0, 100).forEach(k -> {
             InvokeTask worker = InvokeTask.newInstance(() -> {
+                //将count值减1
                 countDownLatch.countDown();
                 System.out.println(countDownLatch.getCount());
                 System.out.println(Thread.currentThread().getName());
             }).invokeTaskName("worker_" + k);
             executor.execute(worker);
         });
-        executor.execute(boss);
     }
 }
