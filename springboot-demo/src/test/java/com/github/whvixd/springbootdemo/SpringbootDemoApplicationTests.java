ab920bee8c8ae6c9c807cfa85091166c6cb2ebd6
wangzhixiang <wangzhixiang_v@didiglobal.com>
2021-04-29 14:19:39 +0800

    springboot-demo

---
+++ springboot-demo/src/test/java/com/github/whvixd/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.whvixd.springbootdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
